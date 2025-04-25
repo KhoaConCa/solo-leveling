d0fe216bd1bb1c2d989e5fe13b2f74b1caf1cdf8
PhuNN <nguyenngocphu2806.working@gmail.com>
2025-04-25 21:05:28 +0700

    Update GUI Mobile handle

--- Assets/Scripts/Player/PlayerStates.cs
+++ Assets/Scripts/Player/PlayerStates.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class demo2 : MonoBehaviour
+public class PlayerStates : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
